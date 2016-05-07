0c5ee4b9a51de9d19f004587eb84d6caa31f285e
Rep2 <ivan.rep2@fer.hr>
2016-05-07 18:09:23 +0200

    Update PollsController.php

--- app/Http/Controllers/PollsController.php
+++ app/Http/Controllers/PollsController.php
@@ -149,10 +149,11 @@ class PollsController extends Controller
             $end_val = Carbon::createFromFormat('Y-m-d H:i:s', $value);
             $end_val->addMinutes(30);
             
-             $answers = DB::raw('SELECT AVG(score), question_id
-                FROM answers
-                WHERE created_at >= "' .$value. '" AND created_at < "' .$end_val->format('Y-m-d H:i:s'). '"
-                GROUP BY question_id');
+             $answers = DB::select(
+                DB::raw('SELECT AVG(score), question_id
+                    FROM answers
+                    WHERE created_at >= "' .$value. '" AND created_at < "' .$end_val->format('Y-m-d H:i:s'). '"
+                    GROUP BY question_id'));
         
             $retVal = [
                 "start" => $value,
